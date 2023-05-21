2ac4a432306f9b27fd1ebc97dbe0f0cfcc34702b
Wilmer ramirez <wil788ramirez@hotmail.com>
2023-05-21 19:24:34 +0200

    cambio de titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export const Header = () => {
   return (
     <div className="text-center my-8">
-        <h1 className=" text-5xl uppercase font-bold ">Proyecto de citas</h1>
+        <h1 className=" text-5xl uppercase font-bold ">Proyecto de citas de pacientes</h1>
         </div>
   )
 }
